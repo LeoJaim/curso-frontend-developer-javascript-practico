cabddfcc6405eb92cbd8945e3985947b5815afb9
Leojaim <leonardo.jaimovich@gmail.com>
2023-12-08 17:08:05 -0300

    Clase 20

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-
+const mobileBurguer = document.querySelector('.menu');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click',toggleDesktopMenu);
-
+mobileBurguer.addEventListener('click',toggleMobileMenu);
 
 function toggleDesktopMenu(){
         desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu(){
+        mobileMenu.classList.toggle('inactive');
 }
 
